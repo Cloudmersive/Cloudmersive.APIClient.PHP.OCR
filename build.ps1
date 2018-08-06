2e8db4b2cce1362746901353369bff936dea07e8
Cloudmersive <35204726+Cloudmersive@users.noreply.github.com>
2018-08-05 22:21:51 -0700

    Added

---
+++ build.ps1
@@ -0,0 +1,12 @@
+﻿#Remove-Item –path ./ –recurse
+& java -jar swagger-codegen-cli.jar generate -i https://api.cloudmersive.com/swagger/api/nlp -l php -c packageconfig.json
+#(Get-Content ./client/package.json).replace('v1', '1.0.1') | Set-Content ./client/package.json
+Copy-Item ./cloudmersive_nlp_api_client/* -Destination . -Recurse -Force
+Remove-Item –path ./cloudmersive_nlp_api_client –recurse
+
+# Bug fix
+
+(Get-Content ./vendor/guzzlehttp/guzzle/src/Client.php).replace("'verify'          => true,", "'verify'          => false,") | Set-Content ./vendor/guzzlehttp/guzzle/src/Client.php
+
+
+& php C:\ProgramData\ComposerSetup\bin\composer.phar install
