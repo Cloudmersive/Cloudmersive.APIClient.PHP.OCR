3b72f637773cc58f4533c0c3e0e6b9415694c106
Cloudmersive <35204726+Cloudmersive@users.noreply.github.com>
2023-07-01 13:55:22 -0700

    3.0.1

--- build.ps1
+++ build.ps1
@@ -14,5 +14,7 @@ Remove-Item –path ./cloudmersive_ocr_api_client –recurse
 (Get-Content ./composer.json).replace('https://github.com/swagger-api/swagger-codegen', 'https://cloudmersive.com') | Set-Content ./composer.json
 (Get-Content ./composer.json).replace('http://swagger.io', 'https://cloudmersive.com') | Set-Content ./composer.json
 #(Get-Content ./composer.json).replace('~1.12', '~2.14.2') | Set-Content ./composer.json
+(Get-Content ./composer.json).replace('^6.2', '^7.5') | Set-Content ./composer.json
+(Get-Content ./composer.json).replace('5.5', '7.2.5') | Set-Content ./composer.json
 
 & C:\tools\php71\php C:\ProgramData\ComposerSetup\bin\composer.phar install
